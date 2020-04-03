de63b61981cff349f64b31bae78ebfe9f1e7d9d6
suryadatta1 <suryadattatangirala@outlook.com>
2020-04-04 00:52:22 +0530

    Added listing articles feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   def show
-    byebug
+    # byebug
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 end
