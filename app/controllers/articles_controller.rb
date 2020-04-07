daa7e3292bd86fbbbdb69eeeb2a1797bd4667be0
suryadatta1 <suryadattatangirala@outlook.com>
2020-04-07 21:44:54 +0530

    showing validations and flash mesaages in html pages while we are creating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,9 +9,16 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
-    render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article created sucessfully."
+      redirect_to @article
+    else
+      render "new"
+    end
   end
 end
